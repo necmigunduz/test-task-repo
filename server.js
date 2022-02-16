1fcf3d73ad4834d39619e545351a59c518f7bf83
necmigunduz <necmigunduz@gmail.com>
2022-02-16 13:36:38 +0300

    Add mutations

--- server.js
+++ server.js
@@ -8,14 +8,22 @@ let schema = buildSchema(`
         product(id: Int!): Product
         products(brand: String): [Product]
     },
+    type Mutation {
+        updateBrand(id: Int!, brand: String): Product
+        updateProductName(id: Int!, productName: String): Product
+        updateColor(id: Int!, color: String): Product
+        updateDescription(id: Int!, description: String): Product
+        updatePrice(id: Int!, price: Int): Product
+        updateAmount(id: Int!, amount: Int): Product
+    },
     type Product {
         id: Int!,
         brand: String,
         productName: String,
         color: String,
         description: String,
-        price: Int!,
-        amount: Int!
+        price: Int,
+        amount: Int
     }
 `);
 
@@ -64,11 +72,77 @@ let getProducts = function(args) {
         return productsData;
     }
 };
+
+let updateBrand = function({id, brand}) {
+    productsData.map(product => {
+        if(product.id === id) {
+            product.brand = brand;
+            return product;
+        }
+    })
+    return productsData.filter(product => product.id === id)[0];
+};
+
+let updateProductName = function({id, productName}) {
+    productsData.map(product => {
+        if(product.id === id) {
+            product.productName = productName;
+            return product;
+        }
+    })
+    return productsData.filter(product => product.id === id)[0];
+};
+
+let updateColor = function({id, color}) {
+    productsData.map(product => {
+        if(product.id === id) {
+            product.color = color;
+            return product;
+        }
+    })
+    return productsData.filter(product => product.id === id)[0];
+};
+
+let updateDescription = function({id, description}) {
+    productsData.map(product => {
+        if(product.id === id) {
+            product.description = description;
+            return product;
+        }
+    })
+    return productsData.filter(product => product.id === id)[0];
+};
+
+let updatePrice = function({id, price}) {
+    productsData.map(product => {
+        if(product.id === id) {
+            product.price = price;
+            return product;
+        }
+    })
+    return productsData.filter(product => product.id === id)[0];
+};
+
+let updateAmount = function({id, amount}) {
+    productsData.map(product => {
+        if(product.id === id) {
+            product.amount = amount;
+            return product;
+        }
+    })
+    return productsData.filter(product => product.id === id)[0];
+};
  
 // Root resolver 
 var root = {
     product: getProduct,
-    products: getProducts
+    products: getProducts,
+    updateBrand: updateBrand,
+    updateProductName: updateProductName,
+    updateColor: updateColor,
+    updateDescription: updateDescription,
+    updatePrice: updatePrice,
+    updateAmount: updateAmount
 };
 
 // Create express server and a GraphQl endpoint
